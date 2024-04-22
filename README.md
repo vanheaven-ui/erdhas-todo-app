5761c40eae1f91cb364b5cd26f8af104ef21cce4
Mworekwa Ezekiel <vanheaven6@gmail.com>
2024-04-22 17:23:04 +0300

    docs:add READM.md

--- README.md
+++ README.md
@@ -4,34 +4,30 @@ A todo application written to demonstrate javascript in action.
 
 ## Description
 
-This involves an attempt to adhere to best practices in Javascript app development. The application persists todos to local Storage.
+This project involves an attempt to adhere to best practices in Javascript app development. The application persists todos to local Storage.
+
+![todo app demo](./todo-demo.gif)
 
 ## Getting Started
 
 ### Dependencies
-
-* There are not dependencies for this project. 
+* There are no dependencies for this project. 
 * You need to have a code editor
 
 ### Installing
-
 * Clone this repository to your local development environment
 
 ### Executing program
-
 * You can use the [liveserver](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) VSCode extension to run the code locally.
 
 
 ## Help
-
 If you have a suggestion, consider adding it to [issues](https://github.com/vanheaven-ui/todo-app-erdhas/issues) tab in the repository.
 
 ## Authors
 
-Contributors names and contact info
-
 Ezekiel Mworekwa  
-[Github](https://github.com/vanheaven-ui)
+[Github](https://github.com/vanheaven-ui) |
 [linkedIn](https://linkedin.com/vanheaven)
 
-## You can leave a ⭐ on the repository if you like it.
+### You can leave a ⭐ on the repository if you like it.
--- script.js
+++ script.js
@@ -1,99 +1,119 @@
-// Grab DOM elements to modify and get data from
+// Grab the DOM elements that we are going to manipulate and those from which we need values
 const todoForm = document.querySelector("form");
 const titleInput = document.getElementById("title");
 const dateInput = document.getElementById("date");
 const tableBody = document.getElementById("todo-list");
 
-// ?? function to create a new Todo row element
-function createTodoElement(todo) {
+// Function that creates a todoRow for each todo
+function createTodoRow(todo) {
   const todoRow = document.createElement("tr");
+  // Mark the table row of a todo as complete or incomplet based on the todos completed status
   if (todo.completed) {
-    todoRow.classList.add("completed-task");
+    todoRow.classList.add("completed-task")
   } else {
-    todoRow.classList.remove("completed-task");
+    todoRow.classList.remove("completed-task")
   }
+  // Create a general tableCell and clone it to create several table cell elements; improve efficiency
   const tableCell = document.createElement("td");
 
-  // Create a title element;
-  const titleElement = tableCell.cloneNode();
+  // Create title Cell
+  const titleElement = tableCell.cloneNode(); // Clones the general tableCell element
   titleElement.innerText = todo.title;
 
-  // Create a date element;
-  const dateElement = tableCell.cloneNode();
+  // Create date Cell
+  const dateElement = tableCell.cloneNode(); // Clones the general tableCell element
   dateElement.innerText = todo.date;
 
-  // Create a checkbox to handle task completion
-  const checkBoxElement = tableCell.cloneNode();
-  const checkBox = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox"); // <input type="checkbox>";
-  checkBox.addEventListener("change", (event) => handleCheckbox(event, todo.id));
+  // Create a checkbox to handle completion status
+  const checkBoxElement = tableCell.cloneNode(); // Clones the general tableCell element
+  // we want to dynamically create <input type="checkbox"> using Javascript
+  const checkBox = document.createElement("input"); 
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.addEventListener("change", (event) => handleChange(event, todo.id));
   checkBox.checked = todo.completed;
   checkBoxElement.appendChild(checkBox);
 
-  // Append the cells to the table row
+  // Append the row elements
   todoRow.append(titleElement, dateElement, checkBoxElement);
 
   return todoRow;
 }
 
-// Function to handle checkBox toggling
-function handleCheckbox(event, id) {
-  const todos = getTodos();
+// Function to handle checkbox change
+function handleChange(event, id) {
+  const todos = getTodos(); // Get todos from local storage
+  // Update the todo that is being modified as completed or incomplete
+  // Use the map method to return a new array with the updated todo
   const newTodos = todos.map((todo) => {
+    // Check if todo is what is being modified
     if (todo.id === id) {
+      // if checkbox is checked, todo should also be completed
       if (event.target.checked) {
-        return { ...todo, completed: true };
-      } else {
-        return { ...todo, completed: false };
+        return {
+          ...todo,
+          completed: true,
+        };
+      } else { // If checkbox is unchecked, todo should be incomplete
+        return {
+          ...todo,
+          completed: false,
+        };
       }
     }
+
+    // Return the rest of the todos as they are to have a full array
     return todo;
   });
-  saveTodos(newTodos);
-  renderTodos()
+
+  saveTodos(newTodos); // Save updated todos to local Storage
+  renderTodos() // Update the UI with the updated todos
 }
 
-// function to handle firm submission
+// Function to handle form submission
 function handleSubmitForm(event) {
-  event.preventDefault();
-  // Validate ad sanitie inputs
-  // create a todo items and add it to the table element
-  const todos = getTodos();
-  const todoItem = {
-    id: new Date().getTime(),
+  event.preventDefault(); // Prevents the default form behaviour the reloads the page when the form is submitted
+
+  const todos = getTodos(); // Get todos from localStorage
+
+  // Update the todos array by adding a new todo object at the end
+  todos.push({
+    id: new Date().getTime(), // Give a unique Id to the todo
     title: titleInput.value,
     date: dateInput.value,
     completed: false,
-  };
+  });
 
-  todos.push(todoItem);
-  saveTodos(todos);
-  renderTodos();
-  // reset form fields so they are ready for new data
+  saveTodos(todos); // Save updated todos in localStorage
+  renderTodos(); // Update the UI with the new Todo
+  // reset the formInput fields so that they are ready for the next input
   todoForm.reset();
 }
 
-// Function to get todod from localstorage
+// Function to get todos from local storage
 function getTodos() {
-  return JSON.parse(localStorage.getItem("todos")) || [];
-}
+  return JSON.parse(localStorage.getItem("todos")) || []; // If no todos in local Storage, return an empty array
+} 
 
-// Function to save todos to local storage
+// Function to save todos  in localmStorage
 function saveTodos(todos) {
-  localStorage.setItem("todos", JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos)); // make sure to stringify whatever is being saved in localStorage
 }
 
-// Function to render todos in the DOM
+// Function to renderTodos
 function renderTodos() {
-  tableBody.innerHTML = "";
+  // Clear out the table body before rerunning the logic to render todos
+  // Avoids creating duplicated todos unnecessarily
+  tableBody.innerHTML = ""; 
+  // Enumerate the todos and render them as rows in the tabke body
   const todos = getTodos();
   todos.forEach((todo) => {
-    const todoRow = createTodoElement(todo);
+    const todoRow = createTodoRow(todo); // returns a table row for each todo
     tableBody.appendChild(todoRow);
   });
 }
 
-renderTodos();
+// This function call updates the UI with todos from localStorage everytime the page loads
+renderTodos(); 
 
-// Listen to submit on the form and modify the DOm
+// Attach an event listenr on the form and listen for submit event and handle that
 todoForm.addEventListener("submit", handleSubmitForm);
